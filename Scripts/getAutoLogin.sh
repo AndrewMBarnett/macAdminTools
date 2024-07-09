f0164333611dbd187b841ca236aa73135d244e5d
Andrew Barnett <138056529+AndrewMBarnett@users.noreply.github.com>
2024-07-09 17:01:39 -0500

    Update getAutoLogin.sh
    added variables

--- Scripts/getAutoLogin.sh
+++ Scripts/getAutoLogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : <<-LICENSE_BLOCK
-getAutoLogin (20220729) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
+setAutoLogin.jamf (20220731) - Copyright (c) 2021 Joel Bruner (https://github.com/brunerd)
 Licensed under the MIT License
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -8,44 +8,54 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 LICENSE_BLOCK
 
+#############
+# VARIABLES #
+#############
+
+#JAMF script parameters are shifted +3
+#provide a username, if blank will disable autologin
+USERNAME="${4}"
+
+#this can be blank if that is the password, it will be verified
+PW="${5}"
 
 #############
 # FUNCTIONS #
 #############
 
-#will XOR it back and truncate padding
-function kcpasswordDecode() (
-	filepath="${1}"
-	#no file
-	if [ -z "${filepath}" ]; then
-		[ ! -t '0' ] && echo "Redirected input no longer supported" >/dev/stderr
-		echo "Please specify a file path" >/dev/stderr
-		exit 1
-	#bad file
-	elif [ ! -f "${filepath}" ]; then
-		echo "$(basename "$0"): ${filepath}: No such file" >/dev/stderr
-		exit 1
-	#file
-	else
-		#test for type of data
-		case "$(file -b "${filepath}")" in
-			#in some cases the kcpassword may be a hex representation in ASCII (10.10+)
-			"ASCII text")
-				#just space out the ASCII data into 2 byte couplets
-				thisStringHex_array=( $(sed 's/../& /g' "${filepath}") )
-				;;
-			#otherwise treat as binary data
-			*)
-				#convert to hex representation with spaces
-				thisStringHex_array=( $(xxd -p -u "${filepath}" | sed 's/../& /g') )
-			;;
-		esac
-	fi
+function jamflog {
+	local logFile="${2:-/var/log/jamf.log}"
+	#if we cannot write to the log or it does not exist, unset and tee simply echoes
+	[ ! -w "${logFile}" ] && unset logFile
+	#this will tee to jamf.log in the jamf log format: <Day> <Month> DD HH:MM:SS <Computer Name> ProcessName[PID]: <Message>
+	echo "$(date +'%a %b %d %H:%M:%S') ${myComputerName:="$(scutil --get ComputerName)"} ${myName:="$(basename "${0}" | sed 's/\..*$//')"}[${myPID:=$$}]: ${1}" | tee -a "${logFile}" 2>/dev/null
+}
+
+#given a string creates data for /etc/kcpassword
+function kcpasswordEncode () (
+
+	#ascii string
+	thisString="${1}"
 
 	#macOS cipher hex ascii representation array
-	cipherHex_array=( 7D 89 52 23 D2 BC DD EA A3 B9 1F )	
+	cipherHex_array=( 7D 89 52 23 D2 BC DD EA A3 B9 1F )
 
-	for ((i=0; i < ${#thisStringHex_array[@]}; i++)); do
+	#converted to hex representation with spaces
+	thisStringHex_array=( $(/bin/echo -n "${thisString}" | xxd -p -u | sed 's/../& /g') )
+
+	#get padding by subtraction if under 12 
+	if [ "${#thisStringHex_array[@]}" -lt 12  ]; then
+		padding=$(( 12 -  ${#thisStringHex_array[@]} ))
+	#get padding by subtracting remainder of modulo 12 if over 12 
+	elif [ "$(( ${#thisStringHex_array[@]} % 12 ))" -ne 0  ]; then
+		padding=$(( (12 - ${#thisStringHex_array[@]} % 12) ))
+	#otherwise even multiples of 12 still need 12 padding
+	else
+		padding=12
+	fi	
+
+	#cycle through each element of the array + padding
+	for ((i=0; i < $(( ${#thisStringHex_array[@]} + ${padding})); i++)); do
 		#use modulus to loop through the cipher array elements
 		charHex_cipher=${cipherHex_array[$(( $i % 11 ))]}
 
@@ -54,14 +64,8 @@ function kcpasswordDecode() (
 	
 		#use $(( shell Aritmethic )) to ^ XOR the two 0x## values (extra padding is 0x00) 
 		#take decimal value and printf convert to two char hex value
-		#use xxd to convert hex to ascii representation
-		decodedCharacter=$(printf "%02X" "$((0x${charHex_cipher} ^ 0x${charHex:-00}))")		
-
-		if [[ "${decodedCharacter}" = "00" ]]; then
-			break
-		else
-			printf "%02X" "$(( 0x${charHex_cipher} ^ 0x${charHex:-00} ))" | xxd -r -p > /dev/stdout
-		fi
+		#use xxd to convert hex to actual value and send to stdout (to avoid NULL issue in bash strings)
+		printf "%02X" "$(( 0x${charHex_cipher} ^ 0x${charHex:-00} ))" | xxd -r -p > /dev/stdout
 	done
 )
 
@@ -69,21 +73,50 @@ function kcpasswordDecode() (
 # MAIN #
 ########
 
-if [ "${UID}" != "0" ]; then
-	echo "Please run as root."
+#quit if not root
+if [ "${UID}" != 0 ]; then
+	jamflog "Please run as root, exiting."
 	exit 1
 fi
 
-#get on auto user
-autoLoginUser=$(/usr/bin/defaults read /Library/Preferences/com.apple.loginwindow autoLoginUser -string "${USERNAME}" 2>/dev/null)
+#special case for Guest account (case SENSITIVE)
+if [ "${USERNAME}" = "Guest" ]; then
+	#turn on Guest account 
+	sysadminctl -guestAccount on
+	#set auto-login
+	defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser Guest
 
-#echo it out or fall back to <NOT_SET>
-echo "Auto login user: ${autoLoginUser:-<NOT_SET>}"
+	echo "Auto login enabled for Guest"
+#if we have any other USERNAME
+elif [ -n "${USERNAME}" ]; then 
 
-if [ -f /etc/kcpassword ]; then
-	echo "Password: $(kcpasswordDecode /etc/kcpassword)"
+	#check user
+	if ! id "${USERNAME}" &> /dev/null; then
+		jamflog "User '${USERNAME}' not found, exiting."
+		exit 1
+	fi
+
+	if ! /usr/bin/dscl /Search -authonly "${USERNAME}" "${PW}" &> /dev/null; then
+		jamflog "Invalid password for '${USERNAME}', exiting."
+		exit 1
+	fi
+
+	#encode password and write directly to file 
+	kcpasswordEncode "${PW}" > /etc/kcpassword
+
+	#ensure ownership and permissions (600)
+	chown root:wheel /etc/kcpassword
+	chmod u=rw,go= /etc/kcpassword
+
+	#turn on auto login
+	/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser -string "${USERNAME}"
+
+	jamflog "Auto login enabled for '${USERNAME}'"
+#if USERNAME empty, turn OFF
 else
-	echo "Password: <NOT_SET>"
+	[ -f /etc/kcpassword ] && rm -f /etc/kcpassword
+	/usr/bin/defaults delete /Library/Preferences/com.apple.loginwindow autoLoginUser &> /dev/null
+	jamflog "Auto login disabled"
 fi
 
 exit 0
